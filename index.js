e2c1048c8050ed550da8b9e3917bfa769772ef67
SuryaNarayana Nistala (MINDTREE LIMITED) <v-sunist@microsoft.com>
2017-02-18 18:45:51 +0530

    added the chunk

--- index.js
+++ index.js
@@ -11,16 +11,31 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
-restService.post('/echo', function(req, res) {
+restService.post('/echo', function (req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var http = require('http');
+    var response;
+    var options = {
+        host: 'http://alfredapi20170217032800.azurewebsites.net/api/values/Get',
+        port: 80,       
+    };
+
+    http.get(options, function (resp) {
+        resp.on('data', function (chunk) {
+             response=resp;
+        });
+    }).on("error", function (e) {
+        console.log("Got error: " + e.message);
+    });
+
     return res.json({
-        speech: req.body.result.source,
+        speech: response[0],
         displayText: speech,
         source: 'webhook-echo-sample'
     });
 });
 
 
-restService.listen((process.env.PORT || 8000), function() {
+restService.listen((process.env.PORT || 8000), function () {
     console.log("Server up and listening");
 });
