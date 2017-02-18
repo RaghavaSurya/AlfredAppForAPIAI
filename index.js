c127f72732f969ae4735343ef3a8cfecb3adee06
SuryaNarayana Nistala (MINDTREE LIMITED) <v-sunist@microsoft.com>
2017-02-18 18:55:31 +0530

    given eating times and menu's

--- index.js
+++ index.js
@@ -14,9 +14,23 @@ restService.use(bodyParser.json());
 restService.post('/echo', function (req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     var http = require('http');        
+    var item;
+    if(req.body.result.parameters.EatingTime=="Dinner")
+    {
+        item="IceCream"
+    }
+    else if(req.body.result.parameters.EatingTime=="Lunch")
+    {
+        item="FriedRice"
+    }
+    else if(req.body.result.parameters.EatingTime=="Breakfast")
+    {
+        item="Idly"
+    }
+
 
     return res.json({
-        speech: req.body.result.parameters.EatingTime ,
+        speech: item ,
         displayText: speech,
         source: 'webhook-echo-sample'
     });
