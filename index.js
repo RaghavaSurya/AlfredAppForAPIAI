6b9e68f8321eb15a56cf217fc5924b96575a12fa
SuryaNarayana Nistala (MINDTREE LIMITED) <v-sunist@microsoft.com>
2017-02-20 16:34:26 +0530

    Cancelled everything

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const restService = express();
+var port = process.env.PORT || 8000;
+restService.listen(port);
+console.log(port);
+console.log("Server up and listening");
 
 restService.use(bodyParser.urlencoded({
     extended: true
@@ -13,46 +17,17 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function (req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    var http = require('http');        
-    var item;
-    var FoodTime = req.body.result.parameters.EatingTime;    
-
-    var sql=require('mssql');
-
-    //configuring alfredapi database
-    var config={
-            user : 'AlfredAdmin',
-            password:'@lfred123',
-            server:'alfredapi.database.windows.net',
-            database:'AlfredDatabase'
-    };
-
-    //connect to database
-    sql.connect(config,function(err){
-        if(err) console.log(err);
-
-        //create request object
-        var request=new sql.Request();
-
-        //query to AlfredDatabase
-        request.query('select * from Food where FoodTime='+FoodTime,function(err,recordset){
-           
-            recordset.forEach(function(element) {
-                item += element.Name;
-            }, this);
-
-        });
-    })  ;  
-
+    var http = require('http');
+    var item="CocaCola";
+    var FoodTime = req.body.result.parameters.EatingTime;
 
     return res.json({
-        speech: item ,
+        speech: item,
         displayText: speech,
         source: 'webhook-echo-sample'
     });
 });
 
 
-restService.listen((process.env.PORT || 8000), function () {
-    console.log("Server up and listening");
-});
+
+
