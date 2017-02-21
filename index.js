314c386e01ac47faa1375b777c91d5fb087a68ab
SuryaNarayana Nistala (MINDTREE LIMITED) <v-sunist@microsoft.com>
2017-02-21 10:49:09 +0530

    added post data for orders

--- index.js
+++ index.js
@@ -19,35 +19,40 @@ restService.post('/echo', function (req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again.";
     var http = require('http');
     var FoodTime = req.body.result.parameters.EatingTime;
-    var item="We have everything here";
+    var item = "We have everything here";
     var request = require('request');
+    if (req.body.result.metadata.intentName == "ShowMenu") {
+        var query = 'http://alfredapi20170217032800.azurewebsites.net/api/values?query=' + FoodTime;
+    }
+    else if(req.body.result.metadata.intentName == "order food"){
+        var OrderText= req.body.result.parameters.OrderText;
+        var query = 'http://alfredapi20170217032800.azurewebsites.net/api/Order?query=' + OrderText;
+    }
 
-    var query='http://alfredapi20170217032800.azurewebsites.net/api/values?query='+FoodTime;
-
-    request(query,function (error, response, body) {       
-            item = body;       
-        if(error) item = error;
-         return res.json({
-        speech: item,
-        displayText: speech,
-        source: 'webhook-echo-sample'
-    });    
-    });    
+    request(query, function (error, response, body) {
+        item = body;
+        if (error) item = error;
+        return res.json({
+            speech: item,
+            displayText: speech,
+            source: 'webhook-echo-sample'
+        });
+    });
 });
 
 restService.get('/ss', function (req, res) {
 
     var request = require('request');
-    var item= " ";
-    var speech=" ";
-    request('http://alfredapi20170217032800.azurewebsites.net/api/values?query=Lunch',function (error, response, body) {       
-            item = body;   
-            
-        if(error) item = error;
-       
+    var item = " ";
+    var speech = " ";
+    request('http://alfredapi20170217032800.azurewebsites.net/api/values?query=Lunch', function (error, response, body) {
+        item = body;
+
+        if (error) item = error;
+
     });
 
-    
+
     // var db=require('node-localdb');
     // var food=db('Food.json');
     // food.insert({FoodName:'Gobi Manchuria',Type:'Starter',})
